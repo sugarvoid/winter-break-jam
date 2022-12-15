24b40120e0ccdb1a11b9dfbcb2cabb46c8361cdb
Dave <gamedev@kitargo.com>
2022-12-15 16:00:35 -0600

    Add moving platform

---
+++ game/platform/moving_platform.gd
@@ -0,0 +1,15 @@
+extends StaticBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var velocity: Vector2
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta) -> void:
+	self.position += Vector2(12, -3) * delta
+	
